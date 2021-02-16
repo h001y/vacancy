65e18a5cca4964d0a8b832f803fa6e72981a183a
hoo1y <hoo1y@yandex.ru>
2021-02-16 22:46:20 +0300

    + add getRegion function

--- sources/application/Loader/Loader.php
+++ sources/application/Loader/Loader.php
@@ -10,23 +10,41 @@ class Loader
 
 	public function __construct(){}
 
-	public function load($file)
+	private function getRegion(string $file): string
+    {
+        $filePath = explode('/', $file);
+        $filename = array_pop($filePath);
+        $partFilenames = explode('.', $filename);
+        $regionTypes = ['eu', 'us'];
+        $currentRegion = '';
+        $result = '';
+        foreach($partFilenames as $partFilename){
+            if (in_array($partFilename, $regionTypes)) {
+                $result = $partFilename;
+            }
+        }
+        return $result;
+    }
+
+	public function load(string $file)
     {
 		Logger::getInst()->info("Starting to load file $file");
+		$currentRegion = $this->getRegion($file);
 		$handle = fopen($file, "r");
 		$fileContent = [];
 		while (($data = fgetcsv($handle, "1000", ",")) !== false) {
 			$fileContent[] = $data;
 		}
 		unset($fileContent[0]);
-		$this->parse($fileContent);
+		$this->parse($fileContent, $currentRegion);
 
 		Logger::getInst()->info("File load is finished");
 	}
 
-	private function parse($content)
+	private function parse(array $content, string $region)
         {
         Logger::getInst()->info("Starting to parse file");
+
         $needleFields = [0, 1, 5];
         foreach ($content as $entry){
             $fieldsToInsert = [];
